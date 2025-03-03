34ea97868a797feb5b72f1ccfeeb013bc75b1684
chat2neil <chat2neil@gmail.com>
2025-03-03 20:19:32 +1300

    minor tweak

--- pdf_to_pinecone.py
+++ pdf_to_pinecone.py
@@ -1,4 +1,3 @@
-# %%
 import os
 import time
 from dotenv import load_dotenv
@@ -14,15 +13,11 @@ PINECONE_API_KEY = os.environ.get("PINECONE_API_KEY")
 PDF_FILE = "./data/Graph-Powered_Machine_Learning.pdf"
 PINECONE_INDEX_NAME = os.path.splitext(os.path.basename(PDF_FILE))[0].lower().replace("_", "-").replace(" ", "-")
 
-# %%
-
 print(f"Loading PDF file: {PDF_FILE}")
 pdf_loader = PyPDFLoader(PDF_FILE)
 docs = []
 for doc in pdf_loader.load():
     docs.append(doc)
-
-# %%
 
 pinecone = Pinecone(api_key=PINECONE_API_KEY)
 existing_indexes = [index_info["name"] for index_info in pinecone.list_indexes()]
@@ -42,14 +37,10 @@ index = pinecone.Index(PINECONE_INDEX_NAME)
 embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
 vector_store = PineconeVectorStore(index=index, embedding=embeddings)
 
-# %%
-
 print("Loading data into Pinecone")
 uuids = [str(uuid4()) for _ in range(len(docs))]
 vector_store.add_documents(documents=docs, ids=uuids)
 
-
-# %%
 print("Data loaded.  Now let's do some queries!")
 
 results = vector_store.similarity_search_with_score(
