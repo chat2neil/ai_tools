0f0d03abe947667d8c8cdeaae91ae217817a4746
chat2neil <chat2neil@gmail.com>
2025-03-16 07:26:36 +1300

    tweaks to report generator

--- research_report.py
+++ research_report.py
@@ -1,10 +1,10 @@
 from research_assistant import builder
 
-topic = "How to consume S3 data from Snowflake"
+topic = "How to consume Snowflake data from Amazon Lake Formation, including how to manage data access security.  Use AWS architect, security architect and data engineer roles as personas."
 
 print("Generating research report...")
 graph = builder.compile()
-results = graph.invoke({"topic": topic, "human_feedback": "approve"})
+results = graph.invoke({"topic": topic, "human_feedback": "approve"}, {"recursion_limit": 50})
 content = results["final_report"]
 
 report_file_name = f"/Users/neil/src/ai_tools/reports/{topic.replace(' ', '_').lower()}.md"
